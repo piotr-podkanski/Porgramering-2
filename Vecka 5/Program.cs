b3d658bc510aee842f1e36f8be1af9b36af444ae
piotr-podkanski <piotr.podkanski@elev.ga.ntig.se>
2024-03-20 00:11:30 +0100

    Add V5

--- Vecka 5/Program.cs
+++ Vecka 5/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine(txt);
         }
-    
+
         static void Main(string[] args)
         {
 
@@ -14,12 +14,30 @@
 
             //count();
 
-            //search_mail();
+            //searchMail();
 
-            ai();
+            //ai();
+
+            //intrestCalc(0.5);
+
+            //string[] firstNames = {"Jhon", "Jerramy", "Jake", "Jakob" };
+            //string[] lastNames = { "Momma", "Brown", "White", "Whiter" };
+            //string[][] Users = { firstNames, lastNames };
+
+            //userSys(Users);
+
+            //donation("Jake white", 200);
+
+            //circle(10);
+
+            //print($"the temperature is {temp(20)} fahrenheit");
 
 
+            //print($"Your input was the value {input()}");
+
+         
         }
+
 
         #region övning 6
         static void count()
@@ -42,7 +60,7 @@
 
         }
 
-        static void search_mail()
+        static void searchMail()
         {
             string[] emails_list = { "greger97@gmaail.com", "anders55@hotmail.com", "viktigpete@icloud.com" };
 
@@ -87,9 +105,142 @@
 
         }
 
-    }
+        static void intrestCalc(double intrestValue = 0.05, double startCap = 500)
+        {
+            double currentValue = startCap;
+            double yearAmount = 0;
+            intrestValue += 1;
+            
+            
+            while (currentValue < startCap * 10) {
+                yearAmount += 1;
+                currentValue = currentValue * intrestValue;
+            }
+
+            Console.WriteLine($"It will take you {yearAmount} years  to 10 times double your start capital \n with an intrest of {(intrestValue -1) * 100}% and you will end up with {currentValue}$");
+        }
+
+        static void userSys(string[][] userList)
+        {
+            List<string> userNameList = new List<string>();
+            bool tooMany = false;
+            string userName;
+            if (userList[0].Count() == userList[1].Count())
+            {
+                for (int i = 0; i < userList[0].Count(); i++)
+                {
+                    userName = userList[0][i].Substring(0, 3);
+                    userName += userList[1][i].Substring(0, 3);
+
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        if (userNameList.Contains(userName) == true)
+                        { 
+                            if (j == 1000)
+                            {
+                                tooMany = true;
+                                print("there are too many users like this");
+                                break;
+                            }
+                            userName += j;
+                            break;
+                        }
+                    }
+                    if (tooMany == false)
+                    {
+                        userNameList.Add(userName);
+                        print($"added {userName}");
+                    }
+                }
+                
+                for (int i = 0; i < userNameList.Count(); i++)
+                {
+                    print(userNameList[i]);
+                }
+            }   
+            
+            else
+            {
+                print("a úser is missing a field");
+            }
+
+        }
+
+        #endregion
+
+
+        #region Övning 7
+
+        static void donation(string name, int amount)
+        {
+
+            print($"We have a new donation, {name} has donated {amount}$ to the charity");
+
+        }
+
+        static void circle(double radius)
+        {
+            double pi = 3.1416;
+            double area = Math.Pow(radius, 2) * pi;
+
+            print($"Your area is {area} a.u");
+
+        }
 
 
         #endregion
 
+        #region övning 9
+
+        static double temp(double celcius)
+        {
+
+            double farenheit = (celcius * 9 / 5) + 32;
+
+            return farenheit;
+        }
+
+        static int input()
+        {
+            int value;
+
+            print("Hello please enter a value that you would like to turn into a interger");
+            while (true)
+            {
+
+                try
+                { 
+                
+                    value = int.Parse(Console.ReadLine());
+                    break;
+                }
+
+                catch (System.FormatException)
+                {
+                    print("You cannot use letters!");
+                }
+                
+
+                
+
+            }
+
+            return value;
+            
+        }
+
+        #endregion
+    }
+
+
 }
+        
+
+
+    
+
+
+
+
+
+
