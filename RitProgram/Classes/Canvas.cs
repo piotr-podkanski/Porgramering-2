291a786a0c364b6d0e679c9c59f96f31dc4e5782
piotr-podkanski <piotr.podkanski@elev.ga.ntig.se>
2024-04-21 01:06:15 +0200

    Remove old canvas class, add new

---
+++ RitProgram/Classes/Canvas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RitProgram.Classes
+{
+    internal abstract class Canvas
+    {
+        public Color color;
+        public int size;
+
+        public Pen pen;
+
+        public Canvas(Color color, int size)
+        {
+            this.color = color; 
+            this.size = size;
+            pen = new Pen(color,size);
+        }
+
+        public abstract void Draw(MouseEventArgs e, Point prevLocation, Graphics g);
+
+    }
+}
