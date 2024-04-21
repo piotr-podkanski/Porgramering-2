5ab7e940b2026573106076b1b47ae78e49c6f00a
piotr-podkanski <piotr.podkanski@elev.ga.ntig.se>
2024-04-21 02:22:47 +0200

    work on canvas

--- RitProgram/Classes/Canvas.cs
+++ RitProgram/Classes/Canvas.cs
@@ -1,21 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace RitProgram.Classes
 {
     internal abstract class Canvas
     {
-        public Color color;
-        public int size;
+        //Sätter fält för klassen 
+        private Color Colour;
+        private int Size;
+        private Pen Pen;
 
-        public Pen pen;
+        // Protected används så att bara ärvande klasser kan använda sig av pen
+        protected Pen pen 
+        {
+            get { return Pen; }
+            set { Pen = value; }
+        }
 
+        public Color colour
+        {
+            get { return Colour; }
+            set 
+            { 
+                Colour = value;
+                pen.Color = value;
+            }
+        }
+
+        public int size
+        {
+            get { return Size; }
+            set 
+            { 
+                Size = value;
+                pen.Width = value;
+            }
+        }
+        
         public Canvas(Color color, int size)
         {
-            this.color = color; 
+            this.colour = color; 
             this.size = size;
             pen = new Pen(color,size);
         }
